62ed6021fd03e3d567c1581ee29e3f4e1a0d92ba
Aswathy S <saswathy974@gmail.com>
2026-09-15 13:45:59 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,14 @@
 class Calculator:
+
     def add(self, a, b):
         return a + b
+
     def subtract(self, a, b):
         return a - b
+
     def multiply(self, a, b):
         return a * b
+
     def divide(self, a, b):
         if b == 0:
             return "Division by zero is not allowed"
@@ -12,9 +16,22 @@ class Calculator:
 
 
 calc = Calculator()
+
 num1 = float(input("Enter first number: "))
+operator = input("Enter operator")
 num2 = float(input("Enter second number: "))
-print("Addition =", calc.add(num1, num2))
-print("Subtraction =", calc.subtract(num1, num2))
-print("Multiplication =", calc.multiply(num1, num2))
-print("Division =", calc.divide(num1, num2))
+
+if operator == "+":
+    print("Result =", calc.add(num1, num2))
+
+elif operator == "-":
+    print("Result =", calc.subtract(num1, num2))
+
+elif operator == "*":
+    print("Result =", calc.multiply(num1, num2))
+
+elif operator == "/":
+    print("Result =", calc.divide(num1, num2))
+
+else:
+    print("Invalid operator")
